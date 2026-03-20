e434c0a6385017a78d0e1d5cb90fc15d20aa91bc
navyaanfoundation <navyaanfoundation@gmail.com>
2026-03-20 08:48:51 +0530

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,7 +7,6 @@ import OurWork from "@/components/our-work";
 import GetInvolved from "@/components/get-involved";
 import GallerySection from "@/components/gallery-section";
 import BlogSection from "@/components/blog-section";
-import NewsletterSection from "@/components/newsletter-section";
 import ContactSection from "@/components/contact-section";
 import Footer from "@/components/footer";
 import BackToTop from "@/components/back-to-top";
@@ -24,7 +23,6 @@ export default function Home() {
       <GetInvolved />
       <GallerySection />
       <BlogSection />
-      <NewsletterSection />
       <ContactSection />
       <Footer />
       <BackToTop />
