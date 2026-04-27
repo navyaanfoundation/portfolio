a489e8694d565bdbd0aeb6cc0e07a2cf3d7dcf95
navyaanfoundation <navyaanfoundation@gmail.com>
2026-04-27 08:11:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, useHashLocation } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,7 +9,7 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch hook={useHashLocation}>
+    <Switch>
       <Route path="/" component={Home} />
       <Route path="/events" component={Events} />
       <Route component={NotFound} />
